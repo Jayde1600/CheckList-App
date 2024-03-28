fcd1ca4ebbce2be1572b4a0151a981c542d0860b
Jayde1600 <neomeslane@gmail.com>
2024-03-28 20:13:24 +0200

    Saves the user's tasks and due date to Cloud Firestore Database.

---
+++ app/src/main/java/com/example/checklist_app/OnCloseDialogListener.java
@@ -0,0 +1,9 @@
+package com.example.checklist_app;
+
+import android.content.DialogInterface;
+
+public interface OnCloseDialogListener {
+
+    void onDialogClose(DialogInterface dialogInterface);
+
+}
